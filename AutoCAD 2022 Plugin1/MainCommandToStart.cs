3da638a1c6a59ddd92610bf542cf2b0029990e33
Colhiru <zoomrulezz@gmail.com>
2024-02-04 22:10:29 +0300

    Доделал связи между View и VM. Добавил команды отмены удаления. Необходимо разделить обязанности общей VM для Layout и Viewport, иначе при добавлении функционала будет плохо.

---
+++ AutoCAD 2022 Plugin1/MainCommandToStart.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using static AutoCAD_2022_Plugin1.Working_functions;
+using AcCoreAp = Autodesk.AutoCAD.ApplicationServices.Core.Application;
+using AutoCAD_2022_Plugin1;
+using Field = AutoCAD_2022_Plugin1.Field;
+using AutoCAD_2022_Plugin1.ViewModels;
+using AutoCAD_2022_Plugin1.Views;
+using Autodesk.AutoCAD.Runtime;
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Geometry;
+
+[assembly: CommandClass(typeof(LightProgram.MainCommandToStart))]
+
+namespace LightProgram
+{
+    public class MainCommandToStart
+    {
+        /*
+         * Конфиг:
+         * Плоттер
+         * Цвет поля
+         * Цвет видового экрана
+         * ??? Стандартный формат
+         * ??? Опциональный масштаб (Он будет рассчитываться исходя из размера поля по высоте или ширине)
+         * Глобальный параметр с указанием того, что делать с объектами которые не вписываются на лист
+         * ??? DownScale
+         * 
+         * Сделать:
+         * Функции апдейта полей и видовых экранов
+         * Форма для взаимодействия с этими функциями.
+         * Основная суть этой формы запустить команду Вызова формы - 
+         * а) выбрать объекты (сразу или постфактум)
+         * б) откроется менеджмент - поменять все как нужно
+         * в) перерисовать все исходя из новых параметров --- НУЖЕН КЛАСС ПЕРЕОТРИСОВКИ И ПЕРЕРАСПРЕДЕЛЕНИЯ ЗНАЧЕНИЙ
+         */
+        public static LocationDraw StartLocationDrawing { get; private set; } = LocationDraw.TopLeft;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [CommandMethod("COLHIRU", CommandFlags.UsePickSet)]
+        public static void COLHIRU()
+        {
+            Document AcDocument = AcCoreAp.DocumentManager.MdiActiveDocument;
+            if (AcDocument is null) throw new System.Exception("No active document!");
+            Database AcDatabase = AcDocument.Database;
+            Editor AcEditor = AcDocument.Editor;
+            LayoutManager layManager = LayoutManager.Current;
+            ObjectContextManager OCM = AcDatabase.ObjectContextManager;
+
+            // Создаем форму
+            // Нужно сделать подгрузку конфига для того, чтобы сразу было можно настроить глобальные параметры плагина, например, где 
+            // располагаются точки, какой начальный принтер.
+            // Здесь должен располагаться конфиг, откуда все подгружается
+            // Имитация
+            FieldList.ColorIndexForField = 3;
+            FieldList.ColorIndexForViewport = 4;
+            FL.StartPoint = new Point2d(0.0, 0.0);
+            string plotterNameFromConfig = "Нет";
+
+            /// Особая инициализация формы из клиентского кода 
+            /// (сначала окно, потом передаем в VM параметр формы, потом грузим форму контекстом)
+            CreateLayoutView window = new CreateLayoutView();
+            CreateLayoutVM tempData = new CreateLayoutVM(window);
+            window.DataContext = tempData;
+
+            tempData.PlotterName = plotterNameFromConfig;
+            if (Application.ShowModalWindow(window) != true) return;
+
+            // Получаем выбранные объекты
+            PromptSelectionResult select = AcEditor.SelectImplied();
+            if (select.Status != PromptStatus.OK)
+            {
+                Application.ShowAlertDialog("Объекты не выбраны! Заново!");
+                // select = AcEditor.GetSelection();
+            }
+            ObjectIdCollection objectsIDs = new ObjectIdCollection(select.Value.GetObjectIds());
+
+            string resultNameLayout = tempData.Name;
+            string resultPlotter = tempData.PlotterName;
+            string resultLayoutFormat = tempData.LayoutFormat;
+            string resultScale = tempData.AnnotationScaleObjectsVP;
+
+            // Добавлем новую филду
+            Field field = FL.AddField(resultNameLayout, resultLayoutFormat, resultPlotter) as Field;
+            if (field == null) throw new ArgumentNullException();
+            ViewportInField viewport = field.AddViewport(resultScale, objectsIDs);
+            //viewport.ChangeStartPoint(new Point2d(0, 0));
+
+            if (field.StateInModel == State.NoExist) 
+                field.Draw();
+
+            if (viewport.StateInModel == State.NoExist)
+                viewport.Draw();
+        }
+
+
+        [CommandMethod("Managing", CommandFlags.UsePickSet)]
+        public static void ManagingLayVP()
+        {
+            Document AcDocument = AcCoreAp.DocumentManager.MdiActiveDocument;
+            if (AcDocument is null) throw new System.Exception("No active document!");
+            Database AcDatabase = AcDocument.Database;
+            Editor AcEditor = AcDocument.Editor;
+            LayoutManager layManager = LayoutManager.Current;
+            ObjectContextManager OCM = AcDatabase.ObjectContextManager;
+
+            // Получаем выбранные объекты
+            PromptEntityResult select = AcEditor.GetEntity("Выберите полилинию макета или видового экрана");
+            if (select.Status != PromptStatus.OK)
+            {
+                Application.ShowAlertDialog("Объекты не выбраны. Заново.");
+                return;
+            }
+            ObjectId objectID = select.ObjectId;
+            // Получаем параметры выбранных объектов
+
+            // Подумай над тем как лучше реализовать свойства массивов в публичном или приватном модификаторе.
+            // Так ли важно их скрывать, если они все равно все изменяются
+
+            string NameLayoutObjects = null;
+            string PlotterNameObjects = null;
+            string LayoutFormatObjects = null;
+            string AnnotationScaleObjects = null;
+            WorkObject TypeWorkObject = WorkObject.None;
+
+            foreach (string NameField in FL.GetNames())
+            {
+                Field field = FL.GetField(NameField);
+                if (field.ContourField == objectID)
+                {
+                    NameLayoutObjects = field.NameLayout;
+                    PlotterNameObjects = field.PlotterName;
+                    LayoutFormatObjects = field.LayoutFormat;
+                    TypeWorkObject = WorkObject.Field;
+                    break;
+                }
+                else
+                {
+                    foreach (Identificator id in field.ViewportIdentificators())
+                    {
+                        ViewportInField vp = field.GetViewport(id);
+
+                        if (vp.ContourObjects == objectID)
+                        {
+                            NameLayoutObjects = field.NameLayout;
+                            PlotterNameObjects = field.PlotterName;
+                            LayoutFormatObjects = field.LayoutFormat;
+                            AnnotationScaleObjects = vp.AnnotationScaleViewport;
+                            TypeWorkObject = WorkObject.Viewport;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            // Проверяем что можно работать с выбранным объектом
+            if (TypeWorkObject == WorkObject.None)
+            {
+                Application.ShowAlertDialog("Выбран неправильный объект. Заново.");
+                return;
+            }
+
+            /// Особая инициализация формы из клиентского кода 
+            /// (сначала окно, потом передаем в VM параметр формы, потом грузим форму контекстом)
+            ManageLayoutViewportView window = new ManageLayoutViewportView();
+            ManageLayoutViewportVM manageData = new ManageLayoutViewportVM(window);
+            window.DataContext = manageData;
+
+            manageData.Name = NameLayoutObjects;
+            manageData.LayoutFormat = LayoutFormatObjects;
+            manageData.PlotterName = PlotterNameObjects;
+            manageData.AnnotationScaleObjectsVP = AnnotationScaleObjects;
+            if (Application.ShowModalWindow(window) != true) return;
+
+
+        }
+
+        /// <summary>
+        /// Тест работы второго окна и его функций
+        /// </summary>
+        [CommandMethod("zoomtest", CommandFlags.UsePickSet)]
+        public static void zoomtest()
+        {
+            Document AcDocument = AcCoreAp.DocumentManager.MdiActiveDocument;
+            if (AcDocument is null) throw new System.Exception("No active document!");
+            Database AcDatabase = AcDocument.Database;
+            Editor AcEditor = AcDocument.Editor;
+            LayoutManager layManager = LayoutManager.Current;
+            ObjectContextManager OCM = AcDatabase.ObjectContextManager;
+
+            PromptSelectionResult select = AcEditor.SelectImplied();
+            if (select.Status != PromptStatus.OK)
+            {
+                Application.ShowAlertDialog("Выберите объекты");
+                select = AcEditor.GetSelection();
+            }
+            ObjectIdCollection objectsIDs = new ObjectIdCollection(select.Value.GetObjectIds());
+
+            AcEditor.WriteMessage("Objects selected");
+
+
+            // ZoomToObjects(objectsIDs);
+        }
+    }
+}
---
+++ AutoCAD 2022 Plugin1/ViewModels/ManageLayoutViewportVM.cs
@@ -0,0 +1,291 @@
+﻿using AutoCAD_2022_Plugin1.Models;
+using AutoCAD_2022_Plugin1.Services;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows;
+
+namespace AutoCAD_2022_Plugin1.ViewModels
+{
+    public class ManageLayoutViewportVM : MainVM
+    {
+        public ManageLayoutViewportVM(Window window) : base(window) { }
+
+        #region Properties
+        /// <summary>
+        /// Static model functions to iteration with Autocad
+        /// </summary>
+        private CreateLayoutModel model = new CreateLayoutModel();
+
+        /// <summary>
+        /// Активная вкладка для реализации команд
+        /// </summary>
+        private string _ActiveTab;
+        public string ActiveTab
+        {
+            get
+            {
+                return _ActiveTab;
+            }
+            set
+            {
+                _ActiveTab = value;
+            }
+        }
+
+        /// <summary>
+        /// Формирует список листов для удаления после закрытия окна
+        /// </summary>
+        private ObservableCollection<string> _LayoutToDelete;
+        public ObservableCollection<string> LayoutToDelete
+        {
+            get
+            {
+                _LayoutToDelete = new ObservableCollection<string>();
+                return _LayoutToDelete;
+            }
+            private set { }
+        }
+
+        /// <summary>
+        /// Формирует список видовых экранов для удаления после закрытия окна
+        /// </summary>
+        private ObservableCollection<string> _ViewportToDelete;
+        public ObservableCollection<string> ViewportToDelete
+        {
+            get
+            {
+                _ViewportToDelete = new ObservableCollection<string>();
+                return _ViewportToDelete;
+            }
+            private set { }
+        }
+
+        /// <summary>
+        /// Проверка редактирования некоторых частей View
+        /// </summary>
+        public bool EnabledFormsParamatersLayout
+        {
+            get
+            {
+                return !LayoutToDelete.Contains(Name);
+            }
+        }
+
+        /// Взаимодействие с именами макетов
+        private ObservableCollection<string> _NamesLayouts;
+        public ObservableCollection<string> NamesLayouts
+        {
+            get
+            {
+                _NamesLayouts = new ObservableCollection<string>(CreateLayoutModel.FL.GetNames());
+                return _NamesLayouts;
+            }
+        }
+        private string _Name;
+        public string Name
+        {
+            get
+            {
+                return _Name;
+            }
+            set
+            {
+                _Name = value.Trim();
+                _EditName = _Name;
+                OnPropertyChanged(nameof(EditName));
+                OnPropertyChanged(nameof(LayoutFormat));
+                OnPropertyChanged(nameof(PlotterName));
+                OnPropertyChanged(nameof(EnabledFormsParamatersLayout));
+            }
+        }
+        private string _EditName;
+        public string EditName
+        {
+            get
+            {
+                return _EditName;
+            }
+            set
+            {
+                _EditName = value.Trim();
+                if (_EditName != _Name)
+                {
+                    OnPropertyChanged(nameof(Name));
+                    OnPropertyChanged(nameof(NamesLayouts));
+                }
+            }
+        }
+
+        /// Взаимодействие с плоттерами
+        private ObservableCollection<string> _Plotters;
+        public ObservableCollection<string> Plotters
+        {
+            get
+            {
+                _Plotters = new ObservableCollection<string>(CreateLayoutModel.GetPlotters());
+                return _Plotters;
+            }
+        }
+        private string _PlotterName;
+        public string PlotterName
+        {
+            get
+            {
+                return _PlotterName;
+            }
+            set
+            {
+                _PlotterName = value;
+                OnPropertyChanged(nameof(Formats));
+            }
+        }
+
+        /// Взаимодействие с форматами
+        private ObservableCollection<string> _Formats;
+        public ObservableCollection<string> Formats
+        {
+            get
+            {
+                _Formats = new ObservableCollection<string>(CreateLayoutModel.GetAllCanonicalScales(_PlotterName));
+                return _Formats;
+            }
+        }
+        private string _LayoutFormat;
+        public string LayoutFormat
+        {
+            get
+            {
+                return _LayoutFormat;
+            }
+            set
+            {
+                _LayoutFormat = value;
+            }
+        }
+
+        /// Взаимодействие видовых экранов
+        private ObservableCollection<string> _Viewports;
+        public ObservableCollection<string> Viewports
+        {
+            get
+            {
+                string[] viewportsID = CreateLayoutModel.FL.GetField(Name).ViewportIdentificators().Select(x => x.ToString()).ToArray();
+                _Viewports = new ObservableCollection<string>(viewportsID);
+                return _Viewports;
+            }
+        }
+        private string _Viewport;
+        public string Viewport
+        {
+            get
+            {
+                return _Viewport;
+            }
+            set
+            {
+                _Viewport = value;
+            }
+        }
+
+        /// Взаимодействие с мастабами видовых экранов
+        private ObservableCollection<string> _Scales;
+        public ObservableCollection<string> Scales
+        {
+            get
+            {
+                _Scales = new ObservableCollection<string>(CreateLayoutModel.GetAllAnnotationScales());
+                return _Scales;
+            }
+        }
+        private string _AnnotationScaleObjectsVP;
+        public string AnnotationScaleObjectsVP
+        {
+            get
+            {
+                return _AnnotationScaleObjectsVP;
+            }
+            set
+            {
+                _AnnotationScaleObjectsVP = value;
+            }
+        }
+
+        #endregion
+
+        #region Commands
+        /// <summary>
+        /// Добавление имени макета в список на удаление
+        /// </summary>
+        private void AddDelete()
+        {
+            switch (ActiveTab) 
+            {
+                case "Layout":
+                    _LayoutToDelete.Add(Name);
+                    break;
+                case "Viewport":
+                    _ViewportToDelete.Add(Name);
+                    break;
+            }
+        }
+        private RelayCommand _DeleteCommand;
+        public RelayCommand DeleteCommand
+        {
+            get
+            {
+                if (_DeleteCommand == null)
+                {
+                    _DeleteCommand = new RelayCommand(o => AddDelete(), null);
+                }
+                return _DeleteCommand;
+            }
+        }
+
+        /// <summary>
+        /// Убрать макета или видовой экран из списка на удаление
+        /// </summary>
+        private void RemoveDelete()
+        {
+            switch (ActiveTab)
+            {
+                case "Layout":
+                    _LayoutToDelete.Remove(Name);
+                    break;
+                case "Viewport":
+                    _ViewportToDelete.Remove(Name);
+                    break;
+            }
+        }
+
+        private RelayCommand _CancelDeleteCommand;
+        public RelayCommand CancelDeleteCommand
+        {
+            get
+            {
+                if (_CancelDeleteCommand == null)
+                {
+                    _CancelDeleteCommand = new RelayCommand(o => RemoveDelete(), null);
+                }
+                return _CancelDeleteCommand;
+            }
+        }
+
+        /// <summary>
+        /// Приблизить на объекты в видовом экране
+        /// </summary>
+        private RelayCommand _ZoomCommand;
+        public RelayCommand ZoomCommand
+        {
+            get
+            {
+                if (_ZoomCommand == null)
+                {
+                    var objectsID = CreateLayoutModel.FL.GetField(Name).GetViewport(Viewport).ObjectsIDs;
+                    _ZoomCommand = new RelayCommand(o => CreateLayoutModel.ZoomToObjects(objectsID), null);
+                }
+                return _ZoomCommand;
+            }
+        }
+        #endregion
+    }
+}
