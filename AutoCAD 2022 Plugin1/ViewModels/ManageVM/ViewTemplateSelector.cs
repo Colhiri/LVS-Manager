7ac6190fb4f28061a679e547047ef70f0e62e95c
Colhiru <zoomrulezz@gmail.com>
2024-02-06 06:51:02 +0300

    Not work

--- AutoCAD 2022 Plugin1/ViewModels/ManageVM/ViewTemplateSelector.cs
+++ AutoCAD 2022 Plugin1/ViewModels/ManageVM/ViewTemplateSelector.cs
@@ -1,8 +1,20 @@
-﻿namespace AutoCAD_2022_Plugin1.ViewModels
-{
-    public class ViewTemplateSelector : ViewTemplateSelector
-    {
+﻿using System.Security.RightsManagement;
+using System.Windows;
+using System.Windows.Controls;
 
+namespace AutoCAD_2022_Plugin1.ViewModels
+{
+    public class ViewTemplateSelector : DataTemplateSelector
+    {
+        
+
+        public override DataTemplate SelectTemplate(object item, DependencyObject dependency)
+        {
+
+
+
+            return new DataTemplate();
+        }
 
 
     }
