d5db33d149513a32a8375b78baaea7dbb38989f0
Colhiru <118454025+Colhiri@users.noreply.github.com>
2024-04-25 11:13:55 +0300

    update v2

--- AutoCAD 2022 Plugin1/Models/CreateLayoutModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using static AutoCAD_2022_Plugin1.CadUtilityLib;
-
-namespace AutoCAD_2022_Plugin1.Models
-{
-    public class CreateLayoutModel : MainModel
-    {
-
-    }
-}
--- AutoCAD 2022 Plugin1/Services/TypeView.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace AutoCAD_2022_Plugin1.Services
-{
-    public enum TypeView
-    {
-        Layout,
-        Viewport
-    }
-}
